55fa198f0d0c8d62b0df8b83f55a0b524173f44e
martin-ngigi <martinwainaina001@gmail.com>
2024-11-27 10:04:39 +0300

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Martin Wainaina on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
